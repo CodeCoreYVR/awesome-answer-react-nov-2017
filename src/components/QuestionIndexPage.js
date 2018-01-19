f463421de8313d1294ffa49b02594fa47fc42248
Steve Godin <steve.god@gmail.com>
2018-01-19 13:22:48 -0800

    Add solution for Question Index Page

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Field} from './Field';
 import questions from '../data/questions';
 
-function QuestionIndexPage (props) {
-  return (
-    <main
-      className="QuestionIndexPage"
-      style={{padding: '0  20px'}}
-    >
-      {/* This is how we comment in JSX! */}
-      <h2>Questions</h2>
-      <ul style={{paddingLeft: '10px'}}>
-        {
-          questions.map(question => (
-            <li key={question.id}>
-              <a href="">{question.title}</a>
-              <Field name="Author" value={question.author.full_name} />
-            </li>
-          ))
-        }
-      </ul>
-    </main>
-  );
+class QuestionIndexPage extends Component {
+  render () {
+    return (
+      <main
+        className="QuestionIndexPage"
+        style={{padding: '0  20px'}}
+      >
+        {/* This is how we comment in JSX! */}
+        <h2>Questions</h2>
+        <ul style={{paddingLeft: '10px'}}>
+          {
+            questions.map(question => (
+              <li key={question.id}>
+                <a href="">{question.title}</a>
+                <Field name="Author" value={question.author.full_name} />
+              </li>
+            ))
+          }
+        </ul>
+      </main>
+    );
+  }
 }
 
 export {QuestionIndexPage};
