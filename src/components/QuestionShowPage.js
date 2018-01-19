137f13c2f8d7c310b43882047f284f1121f5e2e9
Steve Godin <steve.god@gmail.com>
2018-01-19 13:16:40 -0800

    Add demo for Question Show to Class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 // When you're not export a default from a module
 // (e.g. export {QuestionDetails, AnswerDetails}),
 // you must braces after `import` to choose the values
 // that you are import from the module.
 import {QuestionDetails} from './QuestionDetails';
-import {AnswerDetails} from './AnswerDetails';
 import {AnswerList} from './AnswerList';
 import question from '../data/question';
 
-function QuestionShowPage () {
-  const {answers = []} = question;
-  // To pass props to React elements, set them with
-  // "HTML attributes". Each attribute will as a property
-  // of the `props` object.
+class QuestionShowPage extends Component {
+  render () {
+    const {answers = []} = question;
+    // To pass props to React elements, set them with
+    // "HTML attributes". Each attribute will as a property
+    // of the `props` object.
 
-  // When passing props in JSX, any value that
-  // is a non-string (e.g. numbers, objects, arrays, functions, etc)
-  // must be put inside {}.
-  return (
-    <main
-      className="QuestionShowPage"
-      style={{
-        padding: '0 20px'
-      }}
-    >
-      {/* I'm a valid comment */}
-      <QuestionDetails {...question} />
-      <h3>Answer</h3>
-      <AnswerList answers={answers} />
-    </main>
-  );
+    // When passing props in JSX, any value that
+    // is a non-string (e.g. numbers, objects, arrays, functions, etc)
+    // must be put inside {}.
+    return (
+      <main
+        className="QuestionShowPage"
+        style={{
+          padding: '0 20px'
+        }}
+      >
+        {/* I'm a valid comment */}
+        <QuestionDetails {...question} />
+        <h3>Answer</h3>
+        <AnswerList answers={answers} />
+      </main>
+    );
+  }
 }
 
 export {QuestionShowPage};
